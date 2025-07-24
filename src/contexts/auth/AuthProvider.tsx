033dd931a14d80f7deabb28ee2e6ec89b32678aa
joseph mule <josephmule@89569d88-58da-4cbe-b1c4-c6449ac52a87.fritz.box>
2025-07-24 17:53:51 +0200

    🧠 Posizionamento corretto useEffect(clearCacheAndLogout)

--- src/contexts/auth/AuthProvider.tsx
+++ src/contexts/auth/AuthProvider.tsx
@@ -84,8 +84,6 @@ function AuthProvider({ children }: { children: React.ReactNode }) {
   return (
     <AuthContext.Provider
       value={{
-        session,
-
   useEffect(() => {
     clearCacheAndLogout();
   }, [session]);
